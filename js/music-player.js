b879aabdd5a08394b2b57049f3ead9da44ac5888
ymqq127 <s88463617@outlook.com>
2026-09-21 21:41:47 +0800

    Site updated: 2026-09-21 21:41:46

--- js/music-player.js
+++ js/music-player.js
@@ -12,6 +12,13 @@
       url: '/music/%E5%BE%A1%E6%89%8B%E6%B4%97%E4%BA%AE%E5%A4%AA.mp3',
       cover: '/img/music-cover.svg',
       theme: '#FF85A2'
+    },
+    {
+      name: 'BGM 006',
+      artist: 'Unknown',
+      url: '/music/bgm006.ogg',
+      cover: '/img/music-cover.svg',
+      theme: '#FFB6C1'
     }
   ];
 
