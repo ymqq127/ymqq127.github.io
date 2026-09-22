a4d8f48e0c31d6db4f6b1310678477aec6c76124
ymqq127 <s88463617@outlook.com>
2026-09-22 22:37:06 +0800

    Site updated: 2026-09-22 22:37:06

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'pink-traveler-v1';
+const OFFLINE_URL = '/';
+
+const PRECACHE_URLS = [
+  '/',
+  '/css/pink-bundle.css',
+  '/js/main.js',
+  '/js/utils.js',
+  '/img/bg.jpg',
+  '/img/butterfly-icon.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const fetchPromise = fetch(event.request).then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached || caches.match(OFFLINE_URL));
+
+      return cached || fetchPromise;
+    })
+  );
+});
